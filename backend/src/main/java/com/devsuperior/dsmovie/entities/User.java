666e4f42486310ac7bac6af4b56675b5a89a93fd
Adelmo Jr <adelmo.junior0068@gmail.com>
2022-01-16 10:57:53 -0300

    Problem

--- backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,7 +11,8 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_user")
 
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
